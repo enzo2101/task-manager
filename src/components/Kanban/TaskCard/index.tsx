48180889b220635e645f9cee889a4f76cc4785a9
Enzo Ragazzoni <38196071+enzo2101@users.noreply.github.com>
2025-04-10 12:08:26 -0300

    fix: :sparkles: added complete background color and line-clamp

--- src/components/Kanban/TaskCard/index.tsx
+++ src/components/Kanban/TaskCard/index.tsx
@@ -68,7 +68,9 @@ const TaskCard: React.FC<TaskCardProps> = ({ task }) => {
       >
         <div className="flex flex-col gap-2">
           <h1 className="text-base font-semibold">{task.title}</h1>
-          <p className="text-sm text-label">{task.description}</p>
+          <p className="text-sm text-label line-clamp-2 h-fit">
+            {task.description}
+          </p>
         </div>
         <div className="border border-label border-dashed rounded-xl flex justify-between">
           <p className="p-2 text-xs text-label">
