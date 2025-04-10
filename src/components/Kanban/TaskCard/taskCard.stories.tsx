795ab071184ce2af146334f2a51e117165398c7d
Enzo Ragazzoni <38196071+enzo2101@users.noreply.github.com>
2025-04-10 20:01:57 -0300

    refactor: :lipstick: changed default font

--- src/components/Kanban/TaskCard/taskCard.stories.tsx
+++ src/components/Kanban/TaskCard/taskCard.stories.tsx
@@ -16,7 +16,7 @@ export const Last10Days: Story = {
     task: {
       title: "Pesquisa de Mercado",
       description:
-        "Criar um protótipo funcional e detalhado da nova funcionalidade inovadora do produto para uma apresentação impactante...",
+        "Criar um protótipo funcional da nova funcionalidade do produto para apresentação aos stakeholders é uma tarefa crucial que requer atenção aos detalhes e uma execução precisa. O protótipo deve incluir todas as funcionalidades essenciais que demonstram claramente o valor agregado e a inovação que a nova funcionalidade traz para o produto. Além disso, é importante que o protótipo seja intuitivo e fácil de entender, permitindo que os stakeholders visualizem como a funcionalidade será integrada no produto final. A apresentação deve ser bem estruturada, destacando os principais benefícios e mostrando exemplos práticos de uso. A preparação adequada e a prática da apresentação também são essenciais para garantir que todas as perguntas dos stakeholders sejam respondidas de maneira eficaz. Este processo não só facilita a compreensão da nova funcionalidade, mas também ajuda a obter o feedback necessário para ajustes e melhorias antes do lançamento oficial.",
       dueDate: moment().add(10, "days"),
       responsible: ["Enzo"],
       columnId: 1,
@@ -30,7 +30,7 @@ export const Last5Days: Story = {
     task: {
       title: "Pesquisa de Mercado",
       description:
-        "Criar um protótipo funcional e detalhado da nova funcionalidade inovadora do produto para uma apresentação impactante...",
+        "Criar um protótipo funcional e detalhado da nova funcionalidade inovadora do produto para uma apresentação impactante",
       dueDate: moment().add(5, "days"),
       responsible: ["Enzo"],
       columnId: 1,
@@ -44,7 +44,7 @@ export const Last1Days: Story = {
     task: {
       title: "Pesquisa de Mercado",
       description:
-        "Criar um protótipo funcional e detalhado da nova funcionalidade inovadora do produto para uma apresentação impactante...",
+        "Criar um protótipo funcional e detalhado da nova funcionalidade inovadora do produto para uma apresentação impactante",
       dueDate: moment().add(1, "days"),
       responsible: ["Enzo"],
       columnId: 1,
@@ -58,7 +58,7 @@ export const Late1Days: Story = {
     task: {
       title: "Pesquisa de Mercado",
       description:
-        "Criar um protótipo funcional e detalhado da nova funcionalidade inovadora do produto para uma apresentação impactante...",
+        "Criar um protótipo funcional e detalhado da nova funcionalidade inovadora do produto para uma apresentação impactante",
       dueDate: moment().subtract(1, "days"),
       responsible: ["Enzo"],
       columnId: 1,
@@ -72,7 +72,7 @@ export const Late5Days: Story = {
     task: {
       title: "Pesquisa de Mercado",
       description:
-        "Criar um protótipo funcional e detalhado da nova funcionalidade inovadora do produto para uma apresentação impactante...",
+        "Criar um protótipo funcional e detalhado da nova funcionalidade inovadora do produto para uma apresentação impactante",
       dueDate: moment().subtract(5, "days"),
       responsible: ["Enzo"],
       columnId: 1,
@@ -86,7 +86,7 @@ export const Late10Days: Story = {
     task: {
       title: "Pesquisa de Mercado",
       description:
-        "Criar um protótipo funcional e detalhado da nova funcionalidade inovadora do produto para uma apresentação impactante...",
+        "Criar um protótipo funcional e detalhado da nova funcionalidade inovadora do produto para uma apresentação impactante",
       dueDate: moment().subtract(10, "days"),
       responsible: ["Enzo"],
       columnId: 1,
