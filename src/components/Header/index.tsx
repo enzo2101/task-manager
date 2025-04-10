edd025af149a873e8560ce0268892a427ae51192
Enzo Ragazzoni <38196071+enzo2101@users.noreply.github.com>
2025-04-10 16:50:28 -0300

    fix: :art: changed header z index

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header: React.FC = () => {
   return (
-    <div className="bg-white rounded-3xl p-4 flex items-center gap-6 shadow-[0px_4px_14px_0px_rgba(231,_237,_240,_0.3)] w-full">
+    <div className="bg-white rounded-3xl p-4 flex items-center gap-6 z-10 shadow-[0px_4px_14px_0px_rgba(231,_237,_240,_0.3)] w-full">
       <img src="/recrutei-logo.svg" alt="recrutei-logo" />
       <p className="text-main text-sm font-bold">Teste vaga front</p>
     </div>
